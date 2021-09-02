db1c0046031e8b613eec479d83995df83ee39dcb
김현지 <guswlhjk@naver.com>
2021-09-02 18:49:18 +0900

    폰번 작성 화면으로 이동

---
+++ app/src/main/java/com/nepplus/pnonebook_java_20210902/EditPhoneNumActivity.kt
@@ -0,0 +1,21 @@
+package com.nepplus.pnonebook_java_20210902
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPhoneNumActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_phone_num)
+        setUpEvents()
+        setValues()
+    }
+
+    override fun setUpEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/nepplus/pnonebook_java_20210902/MainActivity.kt
+++ app/src/main/java/com/nepplus/pnonebook_java_20210902/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.nepplus.pnonebook_java_20210902
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+//    0.전화번호 추가 기능 -> 별도 화면에서 저장시키자
+
+//    1. 화면에 들어오면 -> 파일에 저장된 이름,폰번,1988-05-05 문장을 불러내자
+//    안드로이드에서 파일 다루는법? + String 분리 / UserData 변환 등등
+
+//    불러낸 데이터들을 -> 리스트뷰에 뿌려주자 (리스트뷰 사용법)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +24,15 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setUpEvents() {
+
+        addPhoneNumBtn.setOnClickListener {
+//            자바의 인터페이스를 개량한 버젼의 코드
+//            EditPhoneNumActivity로 이동 : Intent
+
+            val myIntent = Intent(mContext, EditPhoneNumActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
 
     }
