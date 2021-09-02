f1bbd07c7d4c974a4b1a8e76e48491f9efebc95c
김현지 <guswlhjk@naver.com>
2021-09-02 19:16:59 +0900

    날짜 선택 팝업 띄우기  - 선택 이후의 처리x  - 오늘 날짜를 하드코딩

--- app/src/main/java/com/nepplus/pnonebook_java_20210902/EditPhoneNumActivity.kt
+++ app/src/main/java/com/nepplus/pnonebook_java_20210902/EditPhoneNumActivity.kt
@@ -1,7 +1,11 @@
 package com.nepplus.pnonebook_java_20210902
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import kotlinx.android.synthetic.main.activity_edit_phone_num.*
+import java.util.*
 
 class EditPhoneNumActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +16,33 @@ class EditPhoneNumActivity : BaseActivity() {
     }
 
     override fun setUpEvents() {
-        TODO("Not yet implemented")
+
+        selectBirthDayBtn.setOnClickListener{
+//          달력처럼, 날짜 선택 팝업 출현
+
+            val dateSetListener = object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//            날짜가 선택되면 실행해줄 코드(년/월/일 세개의 Int 가지고 처리)
+//            날짜 선택이 완료되면 (Calendar)되면 -> birthDayTxt에 반영 ( String 반영)
+//            Calendar -> String 가공 : SimpleDataFormat
+                }
+
+            }
+
+            val datePickerDialog = DatePickerDialog(mContext, dateSetListener,
+                2021,Calendar.SEPTEMBER,2)
+
+            datePickerDialog.show()
+
+
+        }
+
+
     }
 
     override fun setValues() {
-        TODO("Not yet implemented")
+
+
     }
 }
